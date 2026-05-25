4b8b0618fe003edd461113f2435d7ac0edcd3e7c
quellus <quellus7@gmail.com>
2026-05-25 18:13:30 -0400

    Fix space chunks radius to stop bleeding into eachother

--- scripts/space_chunk.gd
+++ scripts/space_chunk.gd
@@ -1,7 +1,15 @@
 class_name SpaceChunk extends Node3D
 
-var chunk_size: int = 16
 const SPACE_OBJECT = preload("res://scenes/space_object.tscn")
+
+
+var chunk_size: int = 16:
+	set(value):
+		chunk_size = value
+		chunk_radius = chunk_size/2
+
+var chunk_radius: int = 8
+
 
 var chunk_coord: Vector3i = Vector3i.ZERO:
 	set(value):
@@ -10,15 +18,19 @@ var chunk_coord: Vector3i = Vector3i.ZERO:
 
 var fast_noise_lite: FastNoiseLite = load("res://resources/new_fast_noise_lite.tres")
 
-var rng = RandomNumberGenerator.new()
-
 func generate() -> void:
-	for x in range(-chunk_size, chunk_size):
-		for y in range(-chunk_size, chunk_size):
-			for z in range(-chunk_size, chunk_size):
-				var global_coords: Vector3 = Vector3(global_position.x + x,global_position.y + y,global_position.z + z)
-				var noise_val = fast_noise_lite.get_noise_3dv(global_coords)
-				if  noise_val > 0.80:
-					var space_object = SPACE_OBJECT.instantiate()
-					add_child(space_object);
-					space_object.global_position = global_coords
+	fast_noise_lite.seed = hash(chunk_coord)
+	var avg_cubes = 1
+	for n in range(avg_cubes):
+		var rand_x = n
+		var random_position = Vector3(
+			fast_noise_lite.get_noise_2d(rand_x, 1),
+			fast_noise_lite.get_noise_2d(rand_x, 2),
+			fast_noise_lite.get_noise_2d(rand_x, 3)
+		)
+		var sized_position = random_position * chunk_radius
+		var global_coords: Vector3 = global_position + sized_position
+		var space_object = SPACE_OBJECT.instantiate()
+		add_child(space_object);
+		space_object.add_to_group("SpaceObjects")
+		space_object.global_position = global_coords
